39e99b788cf3a49835d7194adefbea2c94175f3f
Makar <147124109+Refourster@users.noreply.github.com>
2024-05-26 15:18:03 +0500

    Revert "page"
    This reverts commit a996cf507ddf671253fc658f16ed85be7aabba8c.

--- app/games/[id]/page.js
+++ app/games/[id]/page.js
@@ -1,8 +1,16 @@
-import { useStore } from "@/app/store/app-store";
-import { useEffect, useState } from "react";
+"use client";
+import { endpoints } from "../../api/config";
+import {
+  getNormalizedGameDataById,
+  isResponseOk,
+  checkIfUserVoted,
+  vote,
+} from "../../api/api-utils";
 import { GameNotFound } from "@/app/components/GameNotFound/GameNotFound";
 import { Preloader } from "@/app/components/Preloader/Preloader";
-import { getNormalizedGameDataById, isResponseOk, checkIfUserVoted, vote } from "../../api/api-utils";
+import { useState, useEffect } from "react";
+import { useStore } from "@/app/store/app-store";
+
 import Styles from "./Game.module.css";
 
 export default function GamePage(props) {
@@ -13,27 +21,27 @@ export default function GamePage(props) {
 
   useEffect(() => {
     async function fetchData() {
-      const gameData = await getNormalizedGameDataById(endpoints.games, props.params.id);
-      if (isResponseOk(gameData)) {
-        setGame(gameData);
-        setPreloaderVisible(false);
-      } else {
-        setGame(null);
-        setPreloaderVisible(false);
-      }
+      const game = await getNormalizedGameDataById(
+        endpoints.games,
+        props.params.id
+      );
+      isResponseOk(game) ? setGame(game) : setGame(null);
+      setPreloaderVisible(false);
     }
     fetchData();
   }, []);
-
+  
+  
   useEffect(() => {
-    if (authContext.isAuth && game) {
-      async function checkVote() {
-        const voted = await checkIfUserVoted(game, authContext.user.id);
-        setIsVoted(voted);
+    const fetchData = async () => {
+      if (authContext.user && game) {
+        setIsVoted(await checkIfUserVoted(game, authContext.user.id));
+      } else {
+        setIsVoted(false);
       }
-      checkVote();
-    }
-  }, [authContext.isAuth, game]);
+    };
+    fetchData();
+  }, [authContext.user, game]);
 
   const handleVote = async () => {
     const jwt = authContext.token
@@ -57,40 +65,48 @@ export default function GamePage(props) {
     }
   };
 
-  if (preloaderVisible) {
-    return <Preloader />;
-  }
-
-  if (!game) {
-    return <GameNotFound />;
-  }
-
   return (
     <main className="main">
-      <section className={Styles["game"]}>
-        <iframe className={Styles["game__iframe"]} src={game.link}></iframe>
-      </section>
-      <section className={Styles["about"]}>
-        <h2 className={Styles["about__title"]}>{game.title}</h2>
-        <div className={Styles["about__content"]}>
-          <p className={Styles["about__description"]}>{game.description}</p>
-          <div className={Styles["about__author"]}>
-            <p>Автор: <span className={Styles["about__accent"]}>{game.developer}</span></p>
-          </div>
-        </div>
-        <div className={Styles["about__vote"]}>
-          <p className={Styles["about__vote-amount"]}>
-            За игру уже проголосовали: <span className={Styles["about__accent"]}>{game.users.length}</span>
-          </p>
-          <button
-            disabled={!authContext.isAuth || isVoted}
-            className={`button ${Styles["about__vote-button"]}`}
-            onClick={handleVote}
-          >
-            {isVoted ? "Голос учтён" : "Голосовать"}
-          </button>
-        </div>
-      </section>
+      {game ? (
+        <>
+          <section className={Styles["game"]}>
+            <iframe className={Styles["game__iframe"]} src={game.link}></iframe>
+          </section>
+          <section className={Styles["about"]}>
+            <h2 className={Styles["about__title"]}>{game.title}</h2>
+            <div className={Styles["about__content"]}>
+              <p className={Styles["about__description"]}>{game.description}</p>
+              <div className={Styles["about__author"]}>
+                <p>
+                  Автор:{" "}
+                  <span className={Styles["about__accent"]}>
+                    {game.developer}
+                  </span>
+                </p>
+              </div>
+            </div>
+            <div className={Styles["about__vote"]}>
+              <p className={Styles["about__vote-amount"]}>
+                За игру уже проголосовали:{" "}
+                <span className={Styles["about__accent"]}>
+                  {game.users.length}
+                </span>
+              </p>
+              <button
+                disabled={!authContext.isAuth || isVoted}
+                className={`button ${Styles["about__vote-button"]}`}
+                onClick={handleVote}
+              >
+                {isVoted ? "Голос учтён" : "Голосовать"}
+              </button>
+            </div>
+          </section>
+        </>
+      ) : preloaderVisible ? (
+        <Preloader />
+      ) : (
+        <GameNotFound />
+      )}
     </main>
   );
 }
